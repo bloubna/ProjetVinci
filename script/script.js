b92ad8257100f8ca6618db5fbf2fac1541ac0ff0
Emilie Huet <hemilie94@gmail.com>
2018-09-13 17:12:08 +0200

    add sliding effect for languages

--- script/script.js
+++ script/script.js
@@ -1,9 +1,20 @@
-window.onscroll = function () { myFunction() };
+window.onscroll = function() {
+    showUp();
+    showRight();
+}
 
-function myFunction() {
+function showUp() {
     if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
         document.getElementById("showOnScroll").className = "showOnScroll d-flex justify-content-center";
     } else {
         document.getElementById("showOnScroll").className = "hiddenEl";
+    }
+}
+
+function showRight() {
+    if (document.body.scrollTop > 750 || document.documentElement.scrollTop > 750) {
+        document.getElementById("showOnScroll2").className = "showOnScroll2 d-flex justify-content-center w-100";
+    } else {
+        document.getElementById("showOnScroll2").className = "hiddenEl";
     }
 }
